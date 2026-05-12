d87226c438e6c3415a4254322a8c86388aee972f
mercysound <adekunleadewolemercy@gmail.com>
2026-05-12 18:44:44 +0100

    fix EditUserModal import casing

--- frontend/src/components/admin/user/Users.jsx
+++ frontend/src/components/admin/user/Users.jsx
@@ -5,7 +5,7 @@ import axiosInstance from "../../../utils/axiosInstance";
 import UsersTable from "./UsersTable";
 import AddUserPanel from "./AddUserPanel";
 import EmailBroadcastPanel from "./EmailBroadcastPanel";
-import EditUserModal from "./EditUserModal";
+import EditUserModal from "./Editusermodal";
 
 const TABS = [
   { id: "list",  label: "Users",  icon: "👥" },
