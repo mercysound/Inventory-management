0af6b9d19ca689e070aafc6c3145fb00f36c9494
mercysound <adekunleadewolemercy@gmail.com>
2026-05-12 18:19:20 +0100

    temp rename css file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import AuthProvider from './context/AuthContext.jsx';
-import './app.css';
+import './temp.css';
 
 
 // IMPORT GOOGLE PROVIDER
