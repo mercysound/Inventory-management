6fc35095f5d80fff8d8f4ac5c43a418abd597e7b
mercysound <adekunleadewolemercy@gmail.com>
2026-06-03 08:42:33 +0100

    fix: add GoogleOAuthProvider wrapper to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,23 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      {/* CartProvider must be INSIDE AuthProvider because it reads useAuth() */}
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <AuthProvider>
+        {/* CartProvider must be INSIDE AuthProvider because it reads useAuth() */}
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
